66b3e4ac7ac2c849e48514e7829e058df7568261
twentysecondproject <89699543+twentysecondproject@users.noreply.github.com>
2022-06-18 18:55:08 +0200

    Update data.py

--- data.py
+++ data.py
@@ -36,16 +36,32 @@ reg = c['name']
 
 try:
     usa = requests.get(f'https://api.vatusa.net/v2/user/{CID}').json()
+    zhu = requests.get(f'https://api.zhuartcc.org/api/users/{CID}').json()
+    cert_list = ["No Certification", "Minor Certification", "Major Certification", "Solo Certification"]
     name = usa['data']['fname']
     lname = usa['data']['lname']
+    del_cert = zhu['del_cert']
+    gnd_cert = zhu['gnd_cert']
+    twr_cert = zhu['twr_cert']
+    app_cert = zhu['app_cert']
+    ctr_cert = zhu['ctr_cert']
+    ocn_cert = zhu['ocn_cert']
 
     print(f"{name} {lname} ({CID}) has a rating of {rating_list[rat]}")
     print(f'Region: {reg}')
     print(f'Division: {div}')
+    facility = usa['data']['facility']
     if division != "BRZ" and division != "CAM" and division != "GBR" and division != "IL" and division != "JPN" and division != "KOR" and division != "PAC" and division != "PRC" and division != "MCO" and division != "NZ" and division != "ROC" and division != "RUS" and division != "USA" and division != "SSA":
         print(f'Subdivision: {sub}')
+    elif facility == 'ZHU':
+        print(f'Subdivision: {facility}')
+        print(f'DEL Cert: {cert_list[del_cert]}')
+        print(f'GND Cert: {cert_list[gnd_cert]}')
+        print(f'TWR Cert: {cert_list[twr_cert]}')
+        print(f'APP Cert: {cert_list[app_cert]}')
+        print(f'CTR Cert: {cert_list[ctr_cert]}')
+        print(f'OCN Cert: {cert_list[ocn_cert]}')
     elif division == "USA":
-        facility = usa['data']['facility']
         print(f'Subdivision: {facility}')
     else:
         print('Subdivision: None')
