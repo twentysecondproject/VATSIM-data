a97ce73dc8d15b5f11aae81d1b83d96cb07ae204
twentysecondproject <89699543+twentysecondproject@users.noreply.github.com>
2022-06-19 16:45:22 +0200

    Update data.py

--- data.py
+++ data.py
@@ -23,7 +23,7 @@ def get_user_data():
     y = requests.get(f'https://api.vatsim.net/api/ratings/{CID}/rating_times/').json()
 
     cid = CID
-    rating_list = ["Unknown", "OBS", "S1", "S2", "S3", "C1", "C2", "C3", "I1", "I2", "I3", "SUP", "ADM"]
+    rating_list = ["Inactive or Suspended", "OBS", "S1", "S2", "S3", "C1", "C2", "C3", "I1", "I2", "I3", "SUP", "ADM"]
     rat = x['rating']
     region = x['region']
     division = x['division']
